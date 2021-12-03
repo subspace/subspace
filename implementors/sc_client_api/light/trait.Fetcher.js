d5e76bb2e429a0201cc2b1d8f3d10104f22da3a9
nazar-pc <nazar@mokrynskyi.com>
2021-12-03 15:19:49 +0000

    Deploying to gh-pages from  @ 839fff5057bbe2ccfd7546e9574830de16945031 🚀

--- implementors/sc_client_api/light/trait.Fetcher.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["substrate_test_runtime_client"] = [{"text":"impl Fetcher&lt;Block&lt;Header&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>, BlakeTwo256&gt;, <a class=\"enum\" href=\"substrate_test_runtime/enum.Extrinsic.html\" title=\"enum substrate_test_runtime::Extrinsic\">Extrinsic</a>&gt;&gt; for <a class=\"struct\" href=\"substrate_test_runtime_client/struct.LightFetcher.html\" title=\"struct substrate_test_runtime_client::LightFetcher\">LightFetcher</a>","synthetic":false,"types":["substrate_test_runtime_client::LightFetcher"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
