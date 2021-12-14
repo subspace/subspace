988086a94f88726f93015aba9cac85a293d9dbdb
liuchengxu <xuliuchengxlc@gmail.com>
2021-12-14 23:04:09 +0000

    Deploying to gh-pages from  @ 6c9094802f80f203886d66d01a03c148e4b0fde0 🚀

--- sp_executor/sidebar-items.js
+++ sp_executor/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["ExecutorApi","API necessary for executor pallet."]]});
+initSidebarItems({"struct":[["Bundle","Transaction bundle"],["ExecutionReceipt","Receipt of state execution."]],"trait":[["ExecutorApi","API necessary for executor pallet."]],"type":[["BundleHeader","Dummy bundle header."]]});
