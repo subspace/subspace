b7514d0f9c45bf6ed5fb4e0ac6ad2b985c423582
nazar-pc <nazar@mokrynskyi.com>
2022-03-15 11:00:17 +0000

    Deploying to gh-pages from  @ b6d0a018fe1d7450a8d40047dd6f1916935e67f5 🚀

--- subspace_node/sidebar-items.js
+++ subspace_node/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Subcommand","Utilities for working with a node."]],"struct":[["Cli","Subspace Cli."],["ExecutorDispatch","Executor dispatch for subspace runtime"],["RunCmd","Command used to run a Subspace node."]]});
+initSidebarItems({"enum":[["Subcommand","Utilities for working with a node."]],"struct":[["Cli","Subspace Cli."],["ExecutorDispatch","Executor dispatch for subspace runtime"],["ImportBlocksFromDsnCmd","The `import-blocks-from-network` command used to import blocks from Subspace Network DSN."],["RunCmd","Command used to run a Subspace node."]]});
