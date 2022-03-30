cedc97d10dce532ff4979ca9b00c9680b0ea74ad
nazar-pc <nazar@mokrynskyi.com>
2022-03-30 14:55:52 +0000

    Deploying to gh-pages from  @ 87eed0274f9edb2d42681d20166cfbe7bb748ebc 🚀

---
+++ subspace_wasm_tools/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["create_runtime_bundle_inclusion_file","Creates a `target_file_name` in `OUT_DIR` that will contain constant `bundle_const_name` with runtime of `runtime_crate_name` stored in it."]]});
