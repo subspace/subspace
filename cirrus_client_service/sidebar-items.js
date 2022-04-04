9dd4c25b0284118e4eb119267891a4749e237641
nazar-pc <nazar@mokrynskyi.com>
2022-04-04 09:49:08 +0000

    Deploying to gh-pages from  @ ef72770da99e1c0ef792bcfaefe52808712dd566 🚀

--- cirrus_client_service/sidebar-items.js
+++ cirrus_client_service/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["prepare_node_config","Prepare the parachain’s node condifugration"],["start_executor","Start an executor node."],["start_full_node","Start a full node for a parachain."]],"mod":[["genesis",""]],"struct":[["SharedImportQueue","A shared import queue"],["StartExecutorParams","Parameters given to [`start_executor`]."],["StartFullNodeParams","Parameters given to [`start_full_node`]."]]});
+initSidebarItems({"fn":[["start_executor","Start an executor node."]],"struct":[["StartExecutorParams","Parameters given to [`start_executor`]."]]});
