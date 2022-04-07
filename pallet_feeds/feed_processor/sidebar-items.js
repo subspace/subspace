c9702a05b11f2bd8c97733b41426ffe9a27aff14
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-04-07 12:05:19 +0000

    Deploying to gh-pages from  @ ace315ee6112cb9c60ce70936deb3bc9bd1ddbaf 🚀

---
+++ pallet_feeds/feed_processor/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["FeedObjectMapping","Object mapping that points to an object in a block"]],"trait":[["FeedProcessor","FeedProcessor dictates a flow import and constituents of a Feed"]],"type":[["FeedMetadata","Metadata of a feed object as raw bytes."]]});
---
+++ pallet_grandpa_finality_verifier/chain/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["Chain","Minimal Substrate-based chain representation that may be used from no_std environment."]],"type":[["SignedBlock",""]]});
