dbee4bd8ec9ef1f188c57c328721acc7181821f7
nazar-pc <nazar@mokrynskyi.com>
2022-04-08 06:57:45 +0000

    Deploying to gh-pages from  @ 8a61ce0c24ae6bc57017debc32cdb4fb6731146c 🚀

---
+++ implementors/futures_core/stream/trait.FusedStream.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["metered_channel"] = [{"text":"impl&lt;T&gt; FusedStream for <a class=\"struct\" href=\"metered_channel/struct.MeteredReceiver.html\" title=\"struct metered_channel::MeteredReceiver\">MeteredReceiver</a>&lt;T&gt;","synthetic":false,"types":["metered_channel::bounded::MeteredReceiver"]},{"text":"impl&lt;T&gt; FusedStream for <a class=\"struct\" href=\"metered_channel/struct.UnboundedMeteredReceiver.html\" title=\"struct metered_channel::UnboundedMeteredReceiver\">UnboundedMeteredReceiver</a>&lt;T&gt;","synthetic":false,"types":["metered_channel::unbounded::UnboundedMeteredReceiver"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ polkadot_overseer/collation_generation/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["CollationGenerationMessage","Message to the Collation Generation subsystem."]],"struct":[["CollationGenerationSubsystem","Collation Generation Subsystem"]]});
