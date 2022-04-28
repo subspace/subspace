bfd2c87fad2629a77f7d14cddc160f02cdaa7eaf
nazar-pc <nazar@mokrynskyi.com>
2022-04-28 02:18:55 +0000

    Deploying to gh-pages from  @ d117c90ee0d3fc210f8592d9c35c9789c18f9e45 🚀

--- implementors/sp_application_crypto/traits/trait.BoundToRuntimeAppPublic.js
+++ implementors/sp_application_crypto/traits/trait.BoundToRuntimeAppPublic.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
-implementors["pallet_subspace"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_subspace/trait.Config.html\" title=\"trait pallet_subspace::Config\">Config</a>&gt; BoundToRuntimeAppPublic for <a class=\"struct\" href=\"pallet_subspace/struct.Pallet.html\" title=\"struct pallet_subspace::Pallet\">Pallet</a>&lt;T&gt;","synthetic":false,"types":["pallet_subspace::pallet::Pallet"]}];
 implementors["sp_executor"] = [{"text":"impl BoundToRuntimeAppPublic for <a class=\"struct\" href=\"sp_executor/struct.ExecutorKey.html\" title=\"struct sp_executor::ExecutorKey\">ExecutorKey</a>","synthetic":false,"types":["sp_executor::ExecutorKey"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
