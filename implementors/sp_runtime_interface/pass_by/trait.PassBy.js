4c47b8b06205627ded065244aa4353d23c63dd6a
nazar-pc <nazar@mokrynskyi.com>
2022-06-18 19:12:12 +0000

    Deploying to gh-pages from  @ 8d731c246538b6e2c6373686244aad93f6e40200 🚀

--- implementors/sp_runtime_interface/pass_by/trait.PassBy.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["sp_executor"] = [{"text":"impl PassBy for <a class=\"struct\" href=\"sp_executor/struct.FraudProof.html\" title=\"struct sp_executor::FraudProof\">FraudProof</a>","synthetic":false,"types":["sp_executor::FraudProof"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
