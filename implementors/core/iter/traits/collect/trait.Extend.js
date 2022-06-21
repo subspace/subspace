a6c21e5650d2748a08b032f4cfe0545b746eb016
i1i1 <vanyarybin1@live.ru>
2022-06-21 10:39:01 +0000

    Deploying to gh-pages from  @ 36f2e26712cc3484d1df8777b1efa780a0503622 🚀

---
+++ implementors/core/iter/traits/collect/trait.Extend.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["subspace_core_primitives"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.Extend.html\" title=\"trait core::iter::traits::collect::Extend\">Extend</a>&lt;<a class=\"struct\" href=\"subspace_core_primitives/struct.Piece.html\" title=\"struct subspace_core_primitives::Piece\">Piece</a>&gt; for <a class=\"struct\" href=\"subspace_core_primitives/struct.FlatPieces.html\" title=\"struct subspace_core_primitives::FlatPieces\">FlatPieces</a>","synthetic":false,"types":["subspace_core_primitives::FlatPieces"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
