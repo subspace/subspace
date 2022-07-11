5acb5283d657a22f4504bf9e9374442f4ac2a87c
nazar-pc <nazar@mokrynskyi.com>
2022-07-11 11:18:32 +0000

    Deploying to gh-pages from  @ 7e207d9a13c3c8c5b1ec48a36c6a45beb097df23 🚀

--- implementors/core/ops/arith/trait.SubAssign.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["subspace_core_primitives"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a>&lt;<a class=\"struct\" href=\"subspace_core_primitives/struct.U256.html\" title=\"struct subspace_core_primitives::U256\">U256</a>&gt; for <a class=\"struct\" href=\"subspace_core_primitives/struct.U256.html\" title=\"struct subspace_core_primitives::U256\">U256</a>","synthetic":false,"types":["subspace_core_primitives::construct_uint::U256"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
