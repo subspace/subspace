5aa3a9f6cb49f554c74086ae83faf598f095f5d8
i1i1 <vanyarybin1@live.ru>
2022-12-13 12:37:00 +0000

    Deploying to gh-pages from @ subspace/subspace@768a1b59864505f5f14a7e004577f518a5b51c39 🚀

--- implementors/subspace_networking/behavior/custom_record_store/trait.RecordStorage.js
+++ implementors/subspace_networking/behavior/custom_record_store/trait.RecordStorage.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "subspace_networking":[],
-"subspace_service":[["impl&lt;'a, AS&gt; RecordStorage&lt;'a&gt; for <a class=\"struct\" href=\"subspace_service/piece_cache/struct.PieceCache.html\" title=\"struct subspace_service::piece_cache::PieceCache\">PieceCache</a>&lt;AS&gt;<span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;AS: AuxStore + 'a,</span>"]]
+"subspace_service":[["impl&lt;AS:&nbsp;AuxStore&gt; RecordStorage for <a class=\"struct\" href=\"subspace_service/piece_cache/struct.PieceCache.html\" title=\"struct subspace_service::piece_cache::PieceCache\">PieceCache</a>&lt;AS&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
