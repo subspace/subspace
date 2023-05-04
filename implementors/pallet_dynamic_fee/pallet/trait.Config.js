0a7ca42b1b7b0fb2d2be4c232c3d456294958df4
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-05-04 15:06:51 +0000

    Deploying to gh-pages from @ subspace/subspace@629db2fd80120f9947916e7606d064f0f1ff23cc 🚀

--- implementors/pallet_dynamic_fee/pallet/trait.Config.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"core_evm_runtime":[["impl Config for <a class=\"struct\" href=\"core_evm_runtime/struct.Runtime.html\" title=\"struct core_evm_runtime::Runtime\">Runtime</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
