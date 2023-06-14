d99e3b16d38a192693d264fcbd360671e9fde791
NingLin-P <ning@subspace.network>
2023-06-14 15:22:37 +0000

    Deploying to gh-pages from @ subspace/subspace@e57b8d453a5efff44cdb8d4fafd7fcebf344484b 🚀

--- implementors/clap_builder/derive/trait.Subcommand.js
+++ implementors/clap_builder/derive/trait.Subcommand.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"subspace_node":[["impl Subcommand for <a class=\"enum\" href=\"subspace_node/enum.SystemDomainSubcommand.html\" title=\"enum subspace_node::SystemDomainSubcommand\">Subcommand</a>"],["impl Subcommand for <a class=\"enum\" href=\"subspace_node/enum.Subcommand.html\" title=\"enum subspace_node::Subcommand\">Subcommand</a>"]]
+"subspace_node":[["impl Subcommand for <a class=\"enum\" href=\"subspace_node/enum.Subcommand.html\" title=\"enum subspace_node::Subcommand\">Subcommand</a>"],["impl Subcommand for <a class=\"enum\" href=\"subspace_node/enum.SystemDomainSubcommand.html\" title=\"enum subspace_node::SystemDomainSubcommand\">Subcommand</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
