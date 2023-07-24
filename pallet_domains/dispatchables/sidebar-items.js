23921433219d7a4ac7aab917b08539f808820f78
NingLin-P <ning@subspace.network>
2023-07-24 15:10:52 +0000

    Deploying to gh-pages from @ subspace/subspace@561f0e5b2e22e88af18402ca82e784bb9f97c552 🚀

--- pallet_domains/dispatchables/sidebar-items.js
+++ pallet_domains/dispatchables/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["deregister_operator","instantiate_domain","nominate_operator","register_domain_runtime","register_operator","submit_bundle","submit_bundle_v2","submit_fraud_proof","switch_domain","upgrade_domain_runtime","withdraw_stake"]};
+window.SIDEBAR_ITEMS = {"fn":["deregister_operator","instantiate_domain","nominate_operator","register_domain_runtime","register_operator","submit_bundle","submit_fraud_proof","switch_domain","upgrade_domain_runtime","withdraw_stake"]};
--- sp_domains/v2/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["Bundle","BundleHeader","ExecutionReceipt","SealedBundleHeader"],"type":["OpaqueBundle"]};
