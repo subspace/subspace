5ede03d2e6833b4cfb1df40b1f55481e47a78902
nazar-pc <nazar@mokrynskyi.com>
2023-08-07 15:03:27 +0000

    Deploying to gh-pages from @ subspace/subspace@8c078b1d5b6fd660d921c9453d31a2ffc3ce636f 🚀

--- implementors/subspace_networking/behavior/provider_storage/trait.ProviderStorage.js
+++
@@ -1,5 +0,0 @@
-(function() {var implementors = {
-"subspace_farmer":[["impl ProviderStorage for <a class=\"struct\" href=\"subspace_farmer/utils/farmer_provider_storage/struct.FarmerProviderStorage.html\" title=\"struct subspace_farmer::utils::farmer_provider_storage::FarmerProviderStorage\">FarmerProviderStorage</a>"]],
-"subspace_networking":[],
-"subspace_service":[["impl&lt;AS&gt; ProviderStorage for <a class=\"struct\" href=\"subspace_service/piece_cache/struct.PieceCache.html\" title=\"struct subspace_service::piece_cache::PieceCache\">PieceCache</a>&lt;AS&gt;<span class=\"where fmt-newline\">where\n    AS: AuxStore,</span>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/subspace_networking/create/trait.LocalRecordProvider.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"subspace_farmer":[["impl LocalRecordProvider for <a class=\"struct\" href=\"subspace_farmer/piece_cache/struct.PieceCache.html\" title=\"struct subspace_farmer::piece_cache::PieceCache\">PieceCache</a>"]],
+"subspace_networking":[],
+"subspace_service":[["impl&lt;AS&gt; LocalRecordProvider for <a class=\"struct\" href=\"subspace_service/piece_cache/struct.PieceCache.html\" title=\"struct subspace_service::piece_cache::PieceCache\">PieceCache</a>&lt;AS&gt;<span class=\"where fmt-newline\">where\n    AS: AuxStore,</span>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
