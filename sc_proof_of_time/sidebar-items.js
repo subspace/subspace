07d1946ada3a2b72dbfa4123120b209654fd09a6
rahulksnv <78006270+rahulksnv@users.noreply.github.com>
2023-08-08 06:55:23 +0000

    Deploying to gh-pages from @ subspace/subspace@acf255b27ba0b9b0b35fb618f40fef0b85ae7c50 🚀

--- sc_proof_of_time/sidebar-items.js
+++ sc_proof_of_time/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["PotGetBlockProofsError","PotVerifyBlockProofsError"],"fn":["pot_gossip_peers_set_config"],"struct":["ClockMaster","PotClient","PotComponents","PotConfig","PotStateSummary"],"trait":["PotConsensusState"]};
+window.SIDEBAR_ITEMS = {"enum":["PotGetBlockProofsError","PotVerifyBlockProofsError"],"fn":["pot_gossip_peers_set_config"],"struct":["PotClient","PotComponents","PotConfig","PotStateSummary","TimeKeeper"],"trait":["PotConsensusState"]};
