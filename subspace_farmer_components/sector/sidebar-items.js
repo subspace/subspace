189afbf53f632a011c10eb910e836c487b2ef15a
nazar-pc <nazar@mokrynskyi.com>
2023-08-09 07:25:24 +0000

    Deploying to gh-pages from @ subspace/subspace@5dcb2ad5ac95682643b98fa91abd0349405a80c4 🚀

--- subspace_farmer_components/sector/sidebar-items.js
+++ subspace_farmer_components/sector/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SectorContentsMapFromBytesError","SectorContentsMapIterationError"],"fn":["sector_commitments_witnesses_size","sector_record_chunks_size","sector_size"],"struct":["EncodedChunksUsed","RawSector","RecordMetadata","SectorContentsMap","SectorMetadata"]};
+window.SIDEBAR_ITEMS = {"enum":["SectorContentsMapFromBytesError","SectorContentsMapIterationError"],"fn":["sector_record_chunks_size","sector_record_metadata_size","sector_size"],"struct":["EncodedChunksUsed","SectorContentsMap","SectorMetadata"]};
