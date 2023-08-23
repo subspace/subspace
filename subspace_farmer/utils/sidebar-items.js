6dcbba89543438ad18c7d21d5653429c38cf69d9
shamil-gadelshin <shamilgadelshin@gmail.com>
2023-08-23 09:48:23 +0000

    Deploying to gh-pages from @ subspace/subspace@817deb6704767ca159a24395b6ed2126b7bb0239 🚀

--- subspace_farmer/utils/sidebar-items.js
+++ subspace_farmer/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["run_future_in_dedicated_thread"],"mod":["archival_storage_info","archival_storage_pieces","farmer_piece_getter","piece_validator","readers_and_pieces"]};
+window.SIDEBAR_ITEMS = {"fn":["run_future_in_dedicated_thread"],"mod":["farmer_piece_getter","piece_validator","readers_and_pieces"]};
