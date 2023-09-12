90e1d1728e502b4876397258ed9e2e52e2602cf6
shamil-gadelshin <shamilgadelshin@gmail.com>
2023-09-12 14:18:59 +0000

    Deploying to gh-pages from @ subspace/subspace@bd2c41500423b8b94b54f1f930e89d664e61cb49 🚀

---
+++ implementors/actix_web/service/trait.HttpServiceFactory.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"subspace_metrics":[["impl HttpServiceFactory for <a class=\"struct\" href=\"subspace_metrics/struct.metrics.html\" title=\"struct subspace_metrics::metrics\">metrics</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
