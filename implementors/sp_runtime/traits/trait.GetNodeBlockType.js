e82623749a741a8c44211315b62c558f2ed0ac12
nazar-pc <nazar@mokrynskyi.com>
2023-09-13 21:52:01 +0000

    Deploying to gh-pages from @ subspace/subspace@498c4a239ad13c6cc1c0b9d90a12561216ed7a26 🚀

--- implementors/sp_runtime/traits/trait.GetNodeBlockType.js
+++
@@ -1,6 +0,0 @@
-(function() {var implementors = {
-"evm_domain_runtime":[["impl GetNodeBlockType for <a class=\"struct\" href=\"evm_domain_runtime/struct.Runtime.html\" title=\"struct evm_domain_runtime::Runtime\">Runtime</a>"]],
-"evm_domain_test_runtime":[["impl GetNodeBlockType for <a class=\"struct\" href=\"evm_domain_test_runtime/struct.Runtime.html\" title=\"struct evm_domain_test_runtime::Runtime\">Runtime</a>"]],
-"subspace_runtime":[["impl GetNodeBlockType for <a class=\"struct\" href=\"subspace_runtime/struct.Runtime.html\" title=\"struct subspace_runtime::Runtime\">Runtime</a>"]],
-"subspace_test_runtime":[["impl GetNodeBlockType for <a class=\"struct\" href=\"subspace_test_runtime/struct.Runtime.html\" title=\"struct subspace_test_runtime::Runtime\">Runtime</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
