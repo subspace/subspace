1dcd23be50f3280fdd61e7b3a630df2c4e6838e6
nazar-pc <nazar@mokrynskyi.com>
2023-11-22 14:02:49 +0000

    Deploying to gh-pages from @ subspace/subspace@b744fbaa5c83cd3eb62653a2f2f942bf219d426c 🚀

--- subspace_networking/utils/sidebar-items.js
+++ subspace_networking/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["strip_peer_id"],"mod":["multihash","piece_provider"]};
+window.SIDEBAR_ITEMS = {"fn":["strip_peer_id"],"mod":["multihash","piece_provider"],"type":["PeerAddress"]};
