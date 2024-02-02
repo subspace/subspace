8064c24b021a88b1ef2a0509d7ba9e89c268d07e
nazar-pc <nazar@mokrynskyi.com>
2024-02-02 09:27:12 +0000

    Deploying to gh-pages from @ subspace/subspace@466b4feb92d4d4a28f8a50caee4cc9aee0ca349f 🚀

--- trait.impl/sp_consensus_subspace/offence/trait.OnOffenceHandler.js
+++ trait.impl/sp_consensus_subspace/offence/trait.OnOffenceHandler.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"pallet_subspace":[["impl&lt;T: <a class=\"trait\" href=\"pallet_subspace/pallet/trait.Config.html\" title=\"trait pallet_subspace::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"sp_consensus_subspace/offence/trait.OnOffenceHandler.html\" title=\"trait sp_consensus_subspace::offence::OnOffenceHandler\">OnOffenceHandler</a>&lt;Public&gt; for <a class=\"struct\" href=\"pallet_subspace/pallet/struct.Pallet.html\" title=\"struct pallet_subspace::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
+"pallet_subspace":[["impl&lt;T: <a class=\"trait\" href=\"pallet_subspace/pallet/trait.Config.html\" title=\"trait pallet_subspace::pallet::Config\">Config</a>&gt; OnOffenceHandler&lt;Public&gt; for <a class=\"struct\" href=\"pallet_subspace/pallet/struct.Pallet.html\" title=\"struct pallet_subspace::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
 "sp_consensus_subspace":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
